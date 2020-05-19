90186ea14c0874da55ed0e35cb7cfd6794992d17
Aleksander Kostylew <63355626+alkostylew@users.noreply.github.com>
2020-05-19 09:56:10 -0300

    Add files via upload
    Système Multi Agents

---
+++ src/agents/ConsumerAgent.java
@@ -0,0 +1,120 @@
+package agents;
+
+import containers.ConsumerContainer;
+import jade.core.AID;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.ParallelBehaviour;
+import jade.gui.GuiAgent;
+import jade.gui.GuiEvent;
+import jade.lang.acl.ACLMessage;
+
+public class ConsumerAgent extends GuiAgent {
+	
+	private transient ConsumerContainer gui;
+	
+	@Override
+	protected void setup() {
+		if (getArguments().length == 1) {
+			gui = (ConsumerContainer) getArguments()[0];	
+			gui.setConsumerAgent(this);
+		}		
+		
+		ParallelBehaviour parallelBehaviour = new ParallelBehaviour();
+		addBehaviour(parallelBehaviour);
+		
+//		parallelBehaviour.addSubBehaviour(new OneShotBehaviour() {
+//			@Override
+//			public void action() {
+//				System.out.println("Action ...");
+//			}
+//		});
+//		
+//		parallelBehaviour.addSubBehaviour(new CyclicBehaviour() {
+//			@Override
+//			public void action() {
+//				System.out.println("Cyclic ...");
+//			}
+//		});
+//		
+//		parallelBehaviour.addSubBehaviour(new Behaviour() {
+//			private int compteur = 0;
+//			
+//			@Override
+//			public boolean done() {
+//				if (compteur == 10) 
+//					return true;
+//				else 
+//					return false;
+//			}
+//			
+//			@Override
+//			public void action() {
+//				compteur++;
+//				System.out.println("Etape "+compteur);				
+//			}
+//		});
+//		
+//		parallelBehaviour.addSubBehaviour(new TickerBehaviour(this, 1000) {
+//			@Override
+//			protected void onTick() {
+//				System.out.println("TIC");
+//			}
+//		});
+		
+		parallelBehaviour.addSubBehaviour(new CyclicBehaviour() {
+			@Override
+			public void action() {
+				ACLMessage aclMessage = receive();
+				if (aclMessage != null) {
+					System.out.println("*********************************************");
+					System.out.println("Réception du message ");
+					System.out.println(aclMessage.getContent());
+					System.out.println(aclMessage.getSender().getName());
+					System.out.println(aclMessage.getPerformative());
+					System.out.println(aclMessage.getLanguage());
+					System.out.println(aclMessage.getOntology());
+					System.out.println("*********************************************");
+					
+					ACLMessage replay = aclMessage.createReply();
+					replay.setContent("Weekind libre oh non");
+					send(replay);
+				} else {
+					block();
+				}
+			}
+		});
+		
+	}
+	
+	@Override
+	protected void beforeMove() {
+		System.out.println("******************");		
+		System.out.println("Avant Migration ...");
+		System.out.println("******************");		
+	}
+	
+	@Override
+	protected void afterMove() {
+		System.out.println("******************");		
+		System.out.println("Après Migration ...");
+		System.out.println("******************");
+	}
+	
+	@Override
+	protected void takeDown() {
+		System.out.println("******************");		
+		System.out.println("Je suis en train de mourrir ...");
+		System.out.println("******************");
+	}
+
+	@Override
+	public void onGuiEvent(GuiEvent params) {
+		if (params.getType()==1) {
+			String livre = params.getParameter(0).toString();
+			ACLMessage aclMessage = new ACLMessage(ACLMessage.REQUEST);
+			aclMessage.setContent(livre);
+			aclMessage.addReceiver(new AID("rma", AID.ISLOCALNAME));
+			send(aclMessage);
+		}
+	}
+}
---
+++ src/containers/ConsumerContainer.java
@@ -0,0 +1,78 @@
+package containers;
+
+import agents.ConsumerAgent;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.gui.GuiEvent;
+import jade.wrapper.AgentContainer;
+import jade.wrapper.AgentController;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class ConsumerContainer extends Application {
+	
+	protected ConsumerAgent consumerAgent;
+	
+	public static void main(String[] args) throws Exception {
+		launch(args);
+	}
+
+	private void startContainer() throws Exception {
+		Runtime runtime = Runtime.instance();
+		ProfileImpl profileImpl = new ProfileImpl();
+		profileImpl.setParameter(ProfileImpl.MAIN_HOST, "localhost");
+		AgentContainer container = runtime.createAgentContainer(profileImpl);
+		AgentController agentController = container
+				.createNewAgent("Consumer", "agents.ConsumerAgent", new Object[] {this});
+		agentController.start();
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		startContainer();
+		
+		HBox xBox = new HBox();
+		xBox.setPadding(new Insets(10));
+		xBox.setSpacing(10);
+		Label label = new Label("Livre:");
+		TextField textFieldLivre= new TextField();
+		Button buttonAcheter= new Button("Acheter");
+		xBox.getChildren().addAll(label, textFieldLivre, buttonAcheter);
+		
+		VBox vBox = new VBox(); vBox.setPadding(new Insets(10));
+		ObservableList<String> observableList = FXCollections.observableArrayList();
+		ListView<String> listViewMessage = new ListView<String>(observableList);
+		vBox.getChildren().add(listViewMessage);
+		
+		BorderPane borderPane = new BorderPane();
+		borderPane.setTop(xBox);
+		borderPane.setCenter(vBox);
+		Scene scene = new Scene(borderPane, 600, 400);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+		buttonAcheter.setOnAction(evt -> {
+			String livre = textFieldLivre.getText();
+//			observableList.add(livre);
+			GuiEvent event = new GuiEvent(this, 1);
+			event.addParameter(livre);
+			consumerAgent.onGuiEvent(event);
+		});
+	}
+
+	public void setConsumerAgent(ConsumerAgent consumerAgent) {
+		this.consumerAgent = consumerAgent;
+	}
+	
+}
